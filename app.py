7fcf814f0e9c561b8883d1b1f70aaba50e9afdf0
Garvit Bhatnagar <23f1001875@ds.study.iitm.ac.in>
2025-06-19 16:13:37 +0000

    modified previous templates and added base template

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
+@app.route("/")
 @app.route("/login")
 def login_page():
     return render_template('login.html')
@@ -10,9 +11,9 @@ def login_page():
 def admin_page():
     return render_template('admin.html')
 
-@app.route("/base")
-def base():
-    return render_template('base.html')
+@app.route("/user")
+def user_page():
+    return render_template('user.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
