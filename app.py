650b51651d98d7af8db4cf98c554ab5c78b0f255
Garvit Bhatnagar <23f1001875@ds.study.iitm.ac.in>
2025-06-19 10:55:46 +0000

    added admin file and login page file

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello from Flask!"
+@app.route("/login")
+def login_page():
+    return render_template('login.html')
+
+@app.route("/admin")
+def admin_page():
+    return render_template('admin.html')
+
+@app.route("/base")
+def base():
+    return render_template('base.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
